130e10935632855771844e73cb158ab6c4736b85
Attila Oláh <attilaolah@gmail.com>
2014-01-28 11:35:12 +0700

    initial commit

---
+++ permutation.rb
@@ -0,0 +1,45 @@
+class Permutation
+    def initialize(seed, params, bit_length=64)
+        @bit_length = bit_length
+        @mask = (1 << bit_length)-1
+        xorshift = XORShift.new seed, params, @mask
+        @masks = (0.upto bit_length*2).map { |i| xorshift.next & ((1 << (i >> 1)) ^ @mask) }
+    end
+
+    def map(num)
+        do_map num, (0.upto @bit_length-1)
+    end
+
+    def unmap(num)
+        do_map num, ((@bit_length-1).downto 0)
+    end
+
+    private
+    def do_map(num, range)
+        for i in range
+            bit = 1 << i
+            if (bit & num) >> i == 0
+                num ^= @mask ^ (@masks[(i << 1)+((bit & num) >> i)] | (bit ^ bit & num))
+            else
+                num ^= @mask ^ (@masks[(i << 1)+((bit & num) >> i)] | (bit & num))
+            end
+        end
+        num
+    end
+end
+
+private
+class XORShift
+
+    def initialize(seed, params, bitmask)
+        @seed = seed
+        @a, @b, @c = params
+        @bitmask = bitmask
+    end
+
+    def next
+        @seed ^= @bitmask & (@seed << @a)
+        @seed ^= @bitmask & (@seed >> @b)
+        @seed ^= @bitmask & (@seed << @c)
+    end
+end
