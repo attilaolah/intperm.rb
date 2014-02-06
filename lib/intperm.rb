c405744086944336a04de57c330b0f93c4cf9388
Attila Oláh <attilaolah@gmail.com>
2014-02-07 00:05:35 +0700

    rubocop intperm.rb

--- lib/intperm.rb
+++ lib/intperm.rb
@@ -1,39 +1,49 @@
 require 'data'
 
+# Main permutation class.
 class Permutation
-
-  def initialize(seed, bit_length=64)
-    @mask = (1 << bit_length)-1
+  def initialize(seed, bit_length = 64)
+    @mask = (1 << bit_length) - 1
     xorshift = XORShift.new seed, @mask
-    @masks = (0.upto bit_length*2).map { |i| xorshift.next & ((1 << (i >> 1)) ^ @mask) }
+    @masks = (0.upto bit_length * 2).map do |i|
+      xorshift.next & ((1 << (i >> 1)) ^ @mask)
+    end
     @bit_length = bit_length
   end
 
   def map_to(num)
-    do_map num, (0.upto @bit_length-1)
+    permutate num, (0.upto @bit_length - 1)
   end
 
   def map_from(num)
-    do_map num, ((@bit_length-1).downto 0)
+    permutate num, ((@bit_length - 1).downto 0)
   end
 
   private
 
-  def do_map(num, range)
-    for i in range
-      bit = 1 << i
-      if (bit & num) >> i == 0
-        num ^= @mask ^ (@masks[(i << 1)+((bit & num) >> i)] | (bit ^ bit & num))
-      else
-        num ^= @mask ^ (@masks[(i << 1)+((bit & num) >> i)] | (bit & num))
-      end
+  def permutate(num, range)
+    range.each do |i|
+      num ^= bitmask_at_bit num, i
+    end
+    num
+  end
+
+  def bitmask_at_bit(num, bit)
+    bit_mask = 1 << bit
+    if (bit_mask & num) >> bit == 0
+      num = @mask ^ (@masks[(bit << 1) +
+                     ((bit_mask & num) >> bit)] | (bit_mask ^ bit_mask & num))
+    else
+      num = @mask ^ (@masks[(bit << 1) +
+                     ((bit_mask & num) >> bit)] | (bit_mask & num))
     end
     num
   end
 end
 
+# XOR shift helper class.
+# Used for expanding the seed to a random stream.
 class XORShift
-
   def initialize(seed, bitmask)
     @seed = seed
     @a, @b, @c = TRIPLETS[seed % TRIPLETS.length]
